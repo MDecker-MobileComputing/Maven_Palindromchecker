e8f79a17b8d89c641593762c7f4b2a4b1e6a4aa3
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-12-01 18:04:34 +0100

    main-Methode ausimplemenitert

--- src/main/java/de/eldecker/dhbw/palindromchecker/Main.java
+++ src/main/java/de/eldecker/dhbw/palindromchecker/Main.java
@@ -1,10 +1,36 @@
 package de.eldecker.dhbw.palindromchecker;
 
+import static de.eldecker.dhbw.palindromchecker.PalindromChecker.istPalindrom;
 
 public class Main {
     
-    public static void main( String[] args ) {
+    /**
+     * Einstiegsmethode für Palindromcheck.
+     * 
+     * @param args String, der auf Palindromeigenschaft überprüft wird
+     *             (genau ein Wort)
+     */
+    public static void main(String[] args) {
+                
+        if (args.length != 1) {
+            
+            System.out.println("\nFEHLER: Programm nicht mit genau einem Argument aufgerufen\n");
+            System.exit(1);
+        }
         
-        System.out.println( "Hello World!" );
+        try {
+            
+            boolean ergebnis = istPalindrom(args[0]);
+            
+            String ergebnisStr = ergebnis ? "EIN" : "KEIN";
+            
+            String str = String.format("\nDas Wort \"%s\" ist %s Palindrom.\n", args[0], ergebnisStr);
+            System.out.println(str);
+            
+        } catch (PalindromCheckerException e) {
+            
+            System.out.println("\nFEHLER: Exception waehrend Palindrom-Check: " + e.getMessage());
+            System.exit(2);
+        }        
     }
 }
