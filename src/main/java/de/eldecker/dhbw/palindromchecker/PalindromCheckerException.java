9e01c65b9ef6e0679e24472449779d70a28c06ad
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-11-21 09:24:27 +0100

    GH Actions: Versionen aktualisiert

--- src/main/java/de/eldecker/dhbw/palindromchecker/PalindromCheckerException.java
+++ src/main/java/de/eldecker/dhbw/palindromchecker/PalindromCheckerException.java
@@ -4,11 +4,16 @@ package de.eldecker.dhbw.palindromchecker;
 /**
  * Eigene Exception-Klasse für Palindrom-Checker.
  */
+@SuppressWarnings("serial")
 public class PalindromCheckerException extends Exception {
 
+	/**
+	 * Konstruktor für neue Exception. 
+	 * 
+	 * @param message Beschreibung Fehler
+	 */
     public PalindromCheckerException( String message ) {
 
         super( message );
     }
-
 }
